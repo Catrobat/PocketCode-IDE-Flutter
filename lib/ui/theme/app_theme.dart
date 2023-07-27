fc9d79f8dbb76ab9607da126510a99cd694cded1
Adriana Fisselberger <adriana.fisselberger@student.tugraz.at>
2023-07-27 15:31:06 +0200

    Add new color scheme

--- lib/ui/theme/app_theme.dart
+++ lib/ui/theme/app_theme.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
+
+import 'color_schemes.dart';
 part 'colors.dart';
 
 class AppTheme {
   static final ThemeData appTheme = ThemeData(
     useMaterial3: true,
-    colorScheme: ColorScheme.fromSwatch(primarySwatch:  AppColor.primarySwatchColor),
+    colorScheme: lightColorScheme, //ColorScheme.fromSwatch(primarySwatch:  AppColor.primarySwatchColor),
     );//Color(0xff002B3B)),
 }
