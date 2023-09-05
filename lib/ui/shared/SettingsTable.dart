cf9fc47315b7886266679088573f7d25ac4cde28
Adriana Fisselberger <adriana.fisselberger@student.tugraz.at>
2023-09-05 17:14:58 +0200

    Continue settings page

--- lib/ui/shared/SettingsTable.dart
+++ lib/ui/shared/SettingsTable.dart
@@ -1,22 +1,49 @@
 import 'package:flutter/material.dart';
 
 class SettingsTable extends StatelessWidget {
-  const SettingsTable({super.key});
+  const SettingsTable({super.key, isChecked});
+  final bool isChecked = false;
 
   @override
   Widget build(BuildContext context) {
     return Table( //TODO
+      border: TableBorder(bottom: BorderSide(), horizontalInside: BorderSide()),
       defaultVerticalAlignment: TableCellVerticalAlignment.middle,
       children: <TableRow>[
         const TableRow(
           children: <Widget>[
-          const Text("Sprache")
-
+          Text("Sprache \nApp-Sprache auswählen", style: TextStyle(color: Colors.black),),
+          Text("")
           ],
         ),
         const TableRow(
           children: <Widget>[
-          const Text("Webzugriff")
+          Text("Webzugriff \nBearbeiten, um vertrauenswürdeige Domänen hinzuzufügen oder zu entfernen", style: TextStyle(color: Colors.black)),
+          Text("")
+          ],
+        ),
+        const TableRow(
+          children: <Widget>[
+          Text("Mehrspieler-Erweiterung \nErmögliche der App, über Bluetooth freigegebene Multiplayer-Variablen zu verwenden", style: TextStyle(color: Colors.black)),
+          const Checkbox(checkColor: Colors.black, value: true, onChanged: null), //TODO
+          ],
+        ),
+        const TableRow(
+          children: <Widget>[
+            TableCell(child: Text("Sticken \nErstelle Muster für Stickmaschinen", style: TextStyle(color: Colors.black))),
+            TableCell(child: Checkbox(checkColor: Colors.black, focusColor: Colors.black ,value: true, onChanged: null),), //TODO),
+          ],
+        ),
+        const TableRow(
+          children: <Widget>[
+          TableCell(child: Text("Lego Mindstorms NXT Bausteine \nErlaube die Steuerung von Lego Mindstorm NXT Robotern", style: TextStyle(color: Colors.black))),
+          TableCell(child: Text("")),
+          ],
+        ),
+        const TableRow(
+          children: <Widget>[
+            TableCell(child: Text("Lego Mindstorms EV3 Bausteine \nErlaube die Steuerung von Lego Mindstorm EV3 Robotern", style: TextStyle(color: Colors.black)) ),
+            TableCell(child: Text("")),
           ],
         ),
       ],
